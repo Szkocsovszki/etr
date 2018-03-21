a44001e3f8e79e53ef95124f39a3e9f8313326e1
Szkocsovszki Zsolt <szkocsovszki.zsolt@gmail.com>
2018-03-21 14:53:36 +0100

    basics created2

--- ETR/src/Main.java
+++ ETR/src/Main.java
@@ -1,3 +1,8 @@
+import controller.ETRController;
+
 public class Main {
-	
+	public static void main(String args[]) {
+		ETRController controller = new ETRController();
+		controller.startDesktop();
+	}
 }
