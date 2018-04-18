f354c0de196b616b1374b4586bddecacff78717e
Szkocsovszki Zsolt <szkocsovszki.zsolt@gmail.com>
2018-04-18 02:33:50 +0200

    referent new functions

--- ETR/src/view/Labels.java
+++ ETR/src/view/Labels.java
@@ -15,6 +15,7 @@ public class Labels {
 	public static final String CREATE_ACCOUNT = "Fiók létrehozása";
 	public static final String MODIFY_ACCOUNT = "Fiók módosítása";
 	public static final String DELETE_ACCOUNT = "Fiók törlése";
+	public static final String CHANGE_PASSWORD = "Jelszó módosítás";
 	public static final String SIGN_OUT = "Kijelentkezés";
 	public static final String EHA = "EHA kód";
 	public static final String NAME = "Név";
@@ -37,5 +38,14 @@ public class Labels {
 	public static final String EMPTY_ADDRESS_TEXTFIELD = "A Cím mező tartalma nem lehet üres!";
 	public static final String EMPTY_DEPARTMENT_TEXTFIELD = "A Szak mező tartalma nem lehet üres!";
 	public static final String INFORMATION = "Információ";
+	protected static final String DEFAULT_BIRTH_DATE = "YYYY/MM/DD";
+	protected static final String WRONG_DATE_FORMAT = "Rossz dátum formátum!";
+	public static final String DEFAULT_DEPARTMENT = "szak1, szak2, ...";
+	public static final String OLD_PASSWORD = "Régi jelszó";
+	public static final String NEW_PASSWORD = "Új jelszó";
+	public static final String CONFIRM_PASSWORD = "Jelszó megerősítése";
+	public static final String NEW_PASSWORD_MUST_MATCH_CONFIRMED_PASSWORD = "Az új jelszó muszáj, hogy megegyezzen a megerősített jelszóval!";
+	public static final String PASSWORD_SUCCESSFULLY_CHANGED = "Sikeres jelszó módosítás.";
+	public static final String UNSUCCESSFULLY_PASSWORD_CHANGE = "A jelszó megváltoztatása nem sikerült!";
 
 }
