10a0a69bb76d04447ed16f302e267f7ad2f5b3c7
TerhesBalazs <bterhi13@gmail.com>
2018-05-16 14:57:23 +0200

    SQL scripts updated (VIZSGA table + place), Exam was created

--- ETR/src/dao/course/Exam.java
+++ ETR/src/dao/course/Exam.java
@@ -1,5 +1,40 @@
 package dao.course;
 
 public class Exam {
-
+	
+	private String code;
+	private String course;
+	private String time;
+	private String place;
+	private Integer price;
+	
+	public Exam(String code, String course, String time, String place, Integer price) {
+		super();
+		this.code = code;
+		this.course = course;
+		this.time = time;
+		this.place = place;
+		this.price = price;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public String getCourse() {
+		return course;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	
+	public String getPlace() {
+		return place;
+	}
+	
+	public Integer getPrice() {
+		return price;
+	}
+	
 }
